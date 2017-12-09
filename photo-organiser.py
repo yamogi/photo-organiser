8c3f292d4c3e15b314e43a69fabb56f5fd691040
Haydon Murdoch <hgt.murdoch@gmail.com>
2017-12-09 16:03:46 +0000

    Add several functions, format strings in new way

--- photo-organiser.py
+++ photo-organiser.py
@@ -4,21 +4,27 @@ import argparse
 import os
 import sys
 
+def check_dir_exists(directory):
+    try:
+        if not os.path.isdir(directory):
+            raise OSError
+    except OSError:
+        print("The specified directory does not exist: {}".format(directory))
+        sys.exit(1)
+
+def loop_through_dir(directory):
+    for file in os.listdir(directory):
+        if os.path.isfile(file):
+            print("{}/{}".format(directory, file))
+
 parser = argparse.ArgumentParser()
 parser.add_argument("directory", help="the directory to look for photos in")
 args = parser.parse_args()
 
-print("Scanning: %s" % args.directory)
+print("Scanning: {}".format(args.directory))
 print()
 
-try:
-    if not os.path.isdir(args.directory):
-        raise OSError
-except OSError:
-    print("The specified directory does not exist")
-    sys.exit(1)
+check_dir_exists(args.directory)
+loop_through_dir(args.directory)
 
-for file in os.listdir(args.directory):
-    if os.path.isfile(file):
-        print(file)
 

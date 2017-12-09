6e3da3396e09d1bd30eefc1d48ada4a9dbcbdc40
Haydon Murdoch <hgt.murdoch@gmail.com>
2017-12-09 15:56:18 +0000

    Add basic error handling when directory does not exist
    Also add small test block that will loop through files in directory

--- photo-organiser.py
+++ photo-organiser.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("directory", help="the directory to look for photos in")
 args = parser.parse_args()
 
 print("Scanning: %s" % args.directory)
+print()
+
+try:
+    if not os.path.isdir(args.directory):
+        raise OSError
+except OSError:
+    print("The specified directory does not exist")
+    sys.exit(1)
+
+for file in os.listdir(args.directory):
+    if os.path.isfile(file):
+        print(file)
 

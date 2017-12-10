78259c73f488d23667a21d297a887abefb0cf466
Haydon Murdoch <hgt.murdoch@gmail.com>
2017-12-10 17:56:54 +0000

    Remove one space per year/month/day printout

--- photo-organiser.py
+++ photo-organiser.py
@@ -56,9 +56,9 @@ def determine_image_date(date_tag):
     image_month = parsed_dt.month
     image_day = parsed_dt.day
 
-    print(" │    └── Year:  {:>4}".format(parsed_dt.year))
-    print(" │    └── Month: {:>4}".format(parsed_dt.month))
-    print(" │    └── Day    {:>4}".format(parsed_dt.day))
+    print(" │   └── Year:  {:>4}".format(parsed_dt.year))
+    print(" │   └── Month: {:>4}".format(parsed_dt.month))
+    print(" │   └── Day    {:>4}".format(parsed_dt.day))
 
 
 if __name__ == '__main__':
